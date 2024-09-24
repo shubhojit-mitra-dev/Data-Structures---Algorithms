4dfb67fba70fd94d9b00d767c6ba44183ef12c8d
Shubhojit-Mitra <shubhojit.120225@stu.upes.ac.in>
2024-09-24 12:17:12 +0530

    COMPLETE Pattern2

---
+++ Basics/basics.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(string prompt){
+    cout << prompt << endl;
+}
+
+int main() {
+    // string s;
+    // getline(cin, s);
+    // cout << s << endl;
+
+    // DataTypes
+    // if (str == "Character"){
+    //     return 1;
+    // }
+    // else if (str == "Integer"){
+    //     return 4;
+    // }
+    // else if (str == "Long"){
+    //     return 8;
+    // }
+    // else if (str == "Float"){
+    //     return 4;
+    // }
+    // else if (str == "Double"){
+    //     return 8;
+    // }
+    // else {
+    //     return -1;
+    // }
+
+    // If-else
+    // Eligible to Vote
+    // int age;
+    // cin >> age;
+    // if (age >= 18){
+    //     cout << "You are eligible to vote!" << endl;
+    // }
+    // else {
+    //     cout << "You are not eligible to vote!" << endl;
+    // }
+
+    // If marks are less than 25, it prints "Grade: F."
+    // If marks are between 25 and 44 (inclusive), it prints "Grade: E."
+    // If marks are between 45 and 49 (inclusive), it prints "Grade: D."
+    // If marks are between 50 and 59 (inclusive), it prints "Grade: C."
+    // If marks are between 60 and 69 (inclusive), it prints "Grade: B."
+    // If marks are 70 or higher, it prints "Grade: A."
+    // If marks are outside the valid range, it prints "Invalid marks entered."
+
+    // int marks;
+    // cin >> marks;
+    // if (marks < 25 && marks >= 0) {
+    //     cout << "Grade: F" << endl;
+    // } else if (marks >= 25 && marks <= 44) {
+    //     cout << "Grade: E" << endl;
+    // } else if (marks >= 45 && marks <= 49) {
+    //     cout << "Grade: D" << endl;
+    // } else if (marks >= 50 && marks <= 59) {
+    //     cout << "Grade: C" << endl;
+    // } else if (marks >= 60 && marks <= 69) {
+    //     cout << "Grade: B" << endl;
+    // } else if (marks >= 70 && marks <= 100) {
+    //     cout << "Grade: A" << endl;
+    // } else {
+    //     cout << "Invalid marks entered." << endl;
+    // }
+
+    // string FullName;
+    // getline(cin, FullName);
+    // string Modified;
+
+    // int n = FullName.size();
+
+    // for (int i = 0; i < n; i++){
+    //     if (FullName[i] != ' '){
+    //         Modified += FullName[i];
+    //     }
+    // }
+    // cout << Modified << endl;
+
+    
+    // Let's create a print function in c++
+    // Checkout the function above main()
+    string prompt;
+    getline(cin, prompt);
+    print(prompt);
+
+    return 0;
+}
+
