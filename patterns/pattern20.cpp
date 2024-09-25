39c0ce31cb562ab5b628f798e6c9a7fb576df90d
Shubhojit-Mitra <shubhojit.120225@stu.upes.ac.in>
2024-09-25 22:29:12 +0530

    COMPLETE Pattern20

---
+++ patterns/pattern20.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j <= i; j++){
+            cout << "* ";
+        }
+        for (int k = 0; k < 2*n - 2*i - 2; k++){
+            cout << "  ";
+        }
+        for (int j = 0; j <= i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for (int i = 1; i < n; i++){
+        for (int j = 0; j < n-i; j++){
+            cout << "* ";
+        }
+        for (int k = 0; k < 2*i; k++){
+            cout << "  ";
+        }
+        for (int j = 0; j < n-i; j++){
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
